0c6edd5ef1258513a79d7ab616c52e528ae76ad2
MikeWolski <wolski17@gmail.com>
2022-03-02 12:48:10 -0500

    removed container reg

--- appservice/app.tf
+++ appservice/app.tf
@@ -1,12 +1,3 @@
-#Create primary container registry
-resource "azurerm_container_registry" "acr" {
-  name                     = "team4containerregistryprimary"
-  resource_group_name      = "rg-dev-team4-primary"
-  location                 = "eastus"
-  sku                      = "Standard"
-  admin_enabled            = true
-}
-
 #Create primary app service plan
 resource "azurerm_app_service_plan" "web_app_service_plan_primary" {
  name                = "webappserviceplanprimary"
@@ -21,16 +12,7 @@ resource "azurerm_app_service_plan" "web_app_service_plan_primary" {
    capacity = "3"
  }
 }
-/*
-#Create secondary container registry
-resource "azurerm_container_registry" "acr1" {
-  name                     = "team4containerregistrysecondary"
-  resource_group_name      = "rg-dev-team4-secondary"
-  location                 = "centralus"
-  sku                      = "Standard"
-  admin_enabled            = true
-}
-*/
+
 #Create secondary app service plan
 resource "azurerm_app_service_plan" "web_app_service_plan_secondary" {
  name                = "webappserviceplansecondary"
